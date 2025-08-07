be1d3f0a437fe475296a43f6b7013eb55074867c
Connor de la Cruz <connor.c.delacruz@gmail.com>
2025-08-07 17:42:56 -0500

    Update stuff in default loop

--- source/main.lua
+++ source/main.lua
@@ -2,6 +2,8 @@ import 'CoreLibs/object'
 import 'CoreLibs/graphics'
 import 'CoreLibs/sprites'
 import 'CoreLibs/timer'
+import 'CoreLibs/crank'
+import 'CoreLibs/ui'
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
@@ -9,5 +11,8 @@ local gfx <const> = pd.graphics
 ----------------------------------------------------------------------------------
 -- Game Loop
 ----------------------------------------------------------------------------------
+
 function pd.update()
+    gfx.sprite.update()
+    pd.timer.updateTimers()
 end
