b563e5ae174fea67b8326a74d77f2ecc6e2f33c2
Connor de la Cruz <connor.c.delacruz@gmail.com>
2026-01-23 16:10:01 -0600

    import and comment format tweaks

--- source/main.lua
+++ source/main.lua
@@ -1,16 +1,23 @@
+-- Core libs
 import 'CoreLibs/object'
 import 'CoreLibs/graphics'
 import 'CoreLibs/sprites'
+import 'CoreLibs/animation'
+import 'CoreLibs/animator'
 import 'CoreLibs/timer'
+import 'CoreLibs/frameTimer'
 import 'CoreLibs/crank'
 import 'CoreLibs/ui'
+import 'CoreLibs/keyboard'
+-- Toybox-managed libraries
+-- import 'toyboxes'
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
-----------------------------------------------------------------------------------
+-- ===============================================================================
 -- Game Loop
-----------------------------------------------------------------------------------
+-- ===============================================================================
 
 function pd.update()
     gfx.sprite.update()
